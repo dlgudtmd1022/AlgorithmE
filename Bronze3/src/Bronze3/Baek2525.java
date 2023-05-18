46315049479ff1e5cb64a6bb281f25aa4d383054
dlgudtmd <dlgudtmd1022@gmail.com>
2023-05-18 12:23:36 +0900

    Baek 2525

---
+++ Bronze3/src/Bronze3/Baek2525.java
@@ -0,0 +1,29 @@
+package Bronze3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baek2525 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer tk = new StringTokenizer(br.readLine());
+		
+		int A = Integer.parseInt(tk.nextToken());
+		int B = Integer.parseInt(tk.nextToken());
+		tk = new StringTokenizer(br.readLine());
+		int C = Integer.parseInt(tk.nextToken());
+		
+		B += C;
+		if(B >= 60) {			
+			A += B / 60;
+			B %= 60;
+		if(A >= 24) {
+			A %= 24;
+			}		
+		}
+		System.out.println(A + " " + B);
+	}
+}
